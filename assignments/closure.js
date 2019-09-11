9540a7f2e077b69f073b67d00e17e2e54360590f
Samuel Rowan <christbro@gmail.com>
2019-09-10 19:57:31 -0500

    closure done

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,7 +3,14 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
+let thisVariable = true;
 
+function makeFalse(){
+  thisVariable = false;
+  return thisVariable
+}
+makeFalse(thisVariable);
+console.log(thisVariable)
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
